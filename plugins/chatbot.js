512d6c82710635981c1c8631d41f60266999889a
Emergit2006 <127906293+Emergit2006@users.noreply.github.com>
2023-06-07 15:16:17 -0500

    Update chatbot.js

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -15,7 +15,7 @@ conn.reply(m.chat, teks, m, { mentions: { mentionedJid: [m.sender] }})
 
 if (/^@51994156047|@51994156047$/i.test(m.text) ) { //sem prefixo
     let teks = `
-${pickRandom([`Mi Creador me trata muy mal 😭 , Ayuda es el muy na mentira se les agradece.*/n*a todas esas personitas que me siguen prefiriendo Gracias<3`])}
+${pickRandom([`Mi Creador me trata muy mal 😭 , Ayuda es el muy na mentira se les agradece a todas esas personitas que me siguen prefiriendo Gracias<3`])}
 `.trim()
 conn.reply(m.chat, teks, m, { mentions: { mentionedJid: [m.sender] }})
 

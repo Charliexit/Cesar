4b43f8f1022b977bc49050cad295fa3302ade2f4
Emergit2006 <127906293+Emergit2006@users.noreply.github.com>
2023-06-11 04:14:04 -0500

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -5,12 +5,10 @@ handler.before = async (m) => {
 let chat = global.db.data.chats[m.chat]
 if (chat.simi) {
 if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
-if (!m.text) return
 let textodem = m.text  
 try {
-await conn.sendPresenceUpdate('composing', m.chat)
-let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=` + lenguajeGB.lenguaje())
-let data = await ressimi.json();
+let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(textodem)}&lc=es`)
+let data = await ressimi.json();                                                             
 if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return m.reply(`${lol}`) /* EL TEXTO "lol" NO ESTA DEFINIDO PARA DAR ERROR Y USAR LA OTRA API */
 await m.reply(data.success)
 } catch {
